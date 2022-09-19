b210edd5ea9ad9cb7431a5222aa31d989b2041ab
n3dry <daefennek@gmail.com>
2022-09-19 20:42:58 +0200

    LibGUI : Implement diagonal line drawing

--- Userland/LibGUI/Painter.cpp
+++ Userland/LibGUI/Painter.cpp
@@ -21,10 +21,12 @@ void Painter::drawRectangle(int x, int y, Rect size, GUI::Color color)
         }
     }
 }
+
 void Painter::drawQuad(int x, int y, int size, GUI::Color color)
 {
     drawRectangle(x, y, { size, size }, color);
 }
+
 void Painter::drawLine(int x0, int y0, int x1, int y1, GUI::Color color)
 {
     // Vertical Line
@@ -51,7 +53,32 @@ void Painter::drawLine(int x0, int y0, int x1, int y1, GUI::Color color)
         return;
     }
 
-    // TODO Implement diagonal
     // https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    const int dx = ADS::abs(x1 - x0);
+    const int dy = -ADS::abs(y1 - y0);
+    const int sx = x0 < x1 ? 1 : -1;
+    const int sy = y0 < y1 ? 1 : -1;
+    int error = dx + dy;
+    int x = x0;
+    int y = y0;
+
+    while (true) {
+        m_targetBuffer->setPixel(x, y, color);
+        if (x == x1 && y == y1)
+            break;
+        const int error2 = 2 * error;
+        if (error2 >= dy) {
+            if (x == x1)
+                break;
+            error += dy;
+            x += sx;
+        }
+        if (error2 <= dx) {
+            if (y == y1)
+                break;
+            error += dx;
+            y += sy;
+        }
+    }
 }
 } // GUI
