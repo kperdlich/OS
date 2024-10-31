1d1e2a224f56b284271a27dcc01ee3755c2bc91b
Kevin Perdlich <dae_fennek@gmx.de>
2024-10-31 17:52:23 +0100

    Kernel: Properly respect kernel in heap memory section

--- Kernel/MemoryManager.cpp
+++ Kernel/MemoryManager.cpp
@@ -39,28 +39,41 @@ void Initialize(multiboot_info_t& multibootInfo)
     ADS::size_t heapMemorySectionSize = 0;
     for (ADS::size_t i = 0; i < multibootInfo.mmap_length; i += sizeof(multiboot_memory_map_t)) {
         const multiboot_memory_map_t* const memoryMapEntry = reinterpret_cast<multiboot_memory_map_t*>(multibootInfo.mmap_addr + i);
-        const bool sectionContainsKernel = memoryMapEntry->addr < reinterpret_cast<uint32_t>(&kernelEnd)
-            && (memoryMapEntry->addr + memoryMapEntry->len) > reinterpret_cast<uint32_t>(&kernelEnd);
-        // FIXME: This is bit weird! We want to get the biggest available memory region for the heap, but most likely the kernel lives inside it too..
-        if (memoryMapEntry->type == MULTIBOOT_MEMORY_AVAILABLE && memoryMapEntry->len > heapMemorySectionSize && sectionContainsKernel) {
-            heapMemorySection = reinterpret_cast<void*>(memoryMapEntry->addr);
-            heapMemorySectionSize = static_cast<ADS::size_t>(memoryMapEntry->len);
-        }
         dbgPrintf("[MemoryManager] Size: 0x%x | Start: 0x%x | End: 0x%x | Len: %u Bytes | Type: %s\n",
             memoryMapEntry->size,
             static_cast<uint32_t>(memoryMapEntry->addr),
             static_cast<uint32_t>(memoryMapEntry->addr) + static_cast<uint32_t>(memoryMapEntry->len),
             static_cast<uint32_t>(memoryMapEntry->len),
             MemoryMapTypeToString(memoryMapEntry->type));
+        if (memoryMapEntry->type == MULTIBOOT_MEMORY_AVAILABLE && memoryMapEntry->len > heapMemorySectionSize) {
+            heapMemorySection = reinterpret_cast<void*>(memoryMapEntry->addr);
+            heapMemorySectionSize = static_cast<ADS::size_t>(memoryMapEntry->len);
+        }
     }
 
-    dbgPrintf("[MemoryManager] Located Kernel: Memory Section: %p | Ends: %p\n", heapMemorySection, &kernelEnd);
+    ASSERT(heapMemorySection);
+    ASSERT(heapMemorySectionSize > 0);
 
-    const uint32_t kernelEndAligned = alignToPage(reinterpret_cast<uint32_t>(&kernelEnd));
-    ASSERT(reinterpret_cast<uint32_t>(kernelEndAligned) > reinterpret_cast<uint32_t>(&kernelEnd));
-    const uint32_t kernelAlignedSize = kernelEndAligned - reinterpret_cast<uint32_t>(heapMemorySection);
-    void* const heapStart = reinterpret_cast<void*>(kernelEndAligned);
-    const ADS::size_t heapSize = heapMemorySectionSize - kernelAlignedSize;
+    dbgPrintf("[MemoryManager] Found Heap Memory Section | Section: %p | Size: %u Bytes\n", heapMemorySection, heapMemorySectionSize);
+
+    const bool heapSectionContainsKernel = heapMemorySection < &kernelEnd
+        && (static_cast<char*>(heapMemorySection) + heapMemorySectionSize) > reinterpret_cast<void*>(&kernelEnd);
+    void* heapStart = nullptr;
+    ADS::size_t heapSize = {};
+    if (heapSectionContainsKernel) {
+        dbgPrintf("[MemoryManager] Located Kernel in Heap Section - Kernel End: %p\n", &kernelEnd);
+        const uint32_t kernelEndAligned = alignToPage(reinterpret_cast<uint32_t>(&kernelEnd));
+        ASSERT(reinterpret_cast<uint32_t>(kernelEndAligned) > reinterpret_cast<uint32_t>(&kernelEnd));
+        const uint32_t kernelAlignedSize = kernelEndAligned - reinterpret_cast<uint32_t>(heapMemorySection);
+        heapStart = reinterpret_cast<void*>(kernelEndAligned);
+        heapSize = heapMemorySectionSize - kernelAlignedSize;
+    } else {
+        const uint32_t heapMemorySectionAligned = alignToPage(reinterpret_cast<uint32_t>(heapMemorySection));
+        ASSERT(reinterpret_cast<uint32_t>(heapMemorySectionAligned) > reinterpret_cast<uint32_t>(heapMemorySection));
+        const uint32_t alignmentSizeDiff = heapMemorySectionAligned - reinterpret_cast<uint32_t>(heapMemorySection);
+        heapStart = reinterpret_cast<void*>(heapMemorySectionAligned);
+        heapSize = heapMemorySectionSize - alignmentSizeDiff;
+    }
     Heap::initialize(heapStart, heapSize);
 
     dbgPrintf("MemoryManager initialized\n");
